aaf4740553cdd2c87717bdbbc4793494a0111316
Dean McNeil <deanolmcneilgmail.com>
2022-10-07 15:52:51 -0500

    Convert to hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,55 +1,48 @@
-import React, { Component } from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox'
-import Scroll from '../components/Scroll'
-import ErrorBoundary from '../components/ErrorBoundary';
+import React, { useEffect, useState } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
 
-class App extends Component {
-    constructor(){
-        super()
-        this.state = {
-            searchfield: '',
-            robots: []
-        }
-    }
+const App = () => {
+  const [searchfield, setSearchfield] = useState("");
+  const [robots, setRobots] = useState([]);
 
-    render() {
-            const { robots, searchfield } = this.state;
-            const filteredRobots = robots.filter( robot => {
-                return (robot.name.toLowerCase().includes(searchfield.toLowerCase()));
-            })
-            if (!robots.length){
-                return (
-                    <div className='tc'>
-                        <h1 className='f2'>RoboFriends</h1>
-                        <h1 className='f2'>Loading...</h1>
-                    </div>
-                );
-            } else {
-                return(
-                    <div className='tc'>
-                        <h1 className='f2'>RoboFriends</h1>
-                        <SearchBox onSearchChange={this.onSearchChange}/>
-                        <Scroll>
-                            <ErrorBoundary>
-                                <CardList robots={filteredRobots}/>
-                            </ErrorBoundary>
-                        </Scroll>
-                        <h1 className='f4'>Robots delivered by Robohash.org</h1>
-                    </div>
-                );
-            }
-    }
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
+  };
 
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({ robots: users }))
-    }     
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setRobots(users));
+  }, []);
 
-    onSearchChange = (event) =>{
-        this.setState({searchfield:event.target.value})
-    }
-}
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+
+  if (!robots.length) {
+    return (
+      <div className="tc">
+        <h1 className="f2">RoboFriends</h1>
+        <h1 className="f2">Loading...</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div className="tc">
+        <h1 className="f2">RoboFriends</h1>
+        <SearchBox onSearchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
+        </Scroll>
+        <h1 className="f4">Robots delivered by Robohash.org</h1>
+      </div>
+    );
+  }
+};
 
 export default App;
